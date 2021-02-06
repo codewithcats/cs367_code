eafcac963e0f87d3b8d42d87dcdbd1cfa4c406d5
Amp <amp.tanawat@gmail.com>
2021-02-06 10:42:58 +0700

    Extract handle root function

--- week_3_service_implementation/hello/hello-js/hello.js
+++ week_3_service_implementation/hello/hello-js/hello.js
@@ -1,10 +1,16 @@
+// load modules
 const http = require("http");
 
+// code
 console.log("Starting NodeJS server at port 8081");
+
+function handleRoot(req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("Hello from NodeJS!");
+  res.end();
+}
+
 http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("Hello from NodeJS!");
-    res.end();
-  })
+  // handle path "/"
+  .createServer(handleRoot)
   .listen(8081);
