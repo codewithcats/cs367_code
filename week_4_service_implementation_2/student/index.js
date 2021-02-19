e77bba23866d5948a7fea2d07ede7a9dff0d0452
Amp <amp.tanawat@gmail.com>
2021-02-19 21:12:52 +0700

    Student: return dummy students

--- week_4_service_implementation_2/student/index.js
+++ week_4_service_implementation_2/student/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 
+const students = [
+  {
+    id: "4809613106",
+    name: "Tanawat",
+  },
+];
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello" });
+});
+
+app.get("/students", (req, res) => {
+  res.json(students);
 });
 
 app.listen(8000, () => {
